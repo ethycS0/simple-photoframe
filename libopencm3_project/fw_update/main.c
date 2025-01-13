b593a99fed2733e27647a43ebe9e95e5043bf1e3
ethycS0 <arjun.salunkhe@proton.me>
2025-01-13 19:17:13 +0530

    Add HAL/LL directories to pivot project

---
+++ libopencm3_project/fw_update/main.c
@@ -0,0 +1,191 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#define packet_ACK (0x61)
+#define packet_NAK (0x6E)
+#define session_ACK (0x41)
+#define session_NAK (0x4E)
+
+#define MAX_SIZE (0x669A)
+
+unsigned char* generate_crc(unsigned char* data, int length); 
+
+typedef struct packet_t {
+        unsigned int len : 4;
+        unsigned int seqno : 4;
+        uint8_t data[16];
+        uint8_t crc[2];
+} packet_t;
+
+unsigned char* generate_crc(unsigned char* data, int length) {
+        unsigned short crc = 0x1d0f;
+        while(length--) {
+                crc ^= (unsigned short)(*data++) << 8;
+                for(int i = 0; i < 8; i++) {
+                        if(crc & 0x8000) {
+                                crc = (crc << 1) ^ 0x1021;
+                        } else {
+                                crc <<= 1;
+                        }
+                }
+        }
+        unsigned char* result = (unsigned char*)malloc(2 * sizeof(unsigned char));
+        if(result == NULL) {
+                return NULL;
+        }
+
+        result[1] = (unsigned char)(crc & 0x00ff);
+        result[0] = (unsigned char)((crc >> 8) & 0x00ff);
+
+        return result;
+}
+
+
+int main(void) {
+
+        printf("\n +----------------------------------+");
+        printf("\n |         Firmware Update          |");
+        printf("\n +----------------------------------+");
+        printf("\n\n\n");
+
+        // UART Connection Initiate
+
+        int fd;
+        fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY);
+        if (fd == -1) {
+                printf("Could Not Open UART Port \n");
+                return -1;
+        } else if (!isatty(fd)) {
+                printf("Not a tty Device. \n");
+                close(fd);
+                return -1;
+        } else {
+                printf("UART COM Port Opened Successfully \n");
+        }
+
+        // Firmware Select
+
+        FILE *firmware;
+        firmware = fopen("test", "rb");
+        if (firmware == NULL) {
+                printf("Could not Open Firmware.\n");
+                return -1;
+        }
+        fseek(firmware, 0L, SEEK_END);
+        long fsize = ftell(firmware);
+        long total_packets_bytes = (fsize + 15) / 16;
+        if (total_packets_bytes > (long)MAX_SIZE) {
+                printf("Firmware size too large\n");
+                return -1;
+        }
+
+        unsigned char total_packets[2];
+        total_packets[1] = (unsigned char)(total_packets_bytes & 0x00FF);
+        total_packets[0] = (unsigned char)((total_packets_bytes >> 8) & 0x00FF);
+
+        fseek(firmware, 0L, SEEK_SET);
+        // UART configuration
+
+        struct termios options;
+
+        options.c_iflag =
+            IGNPAR; // Ignore parity errors, as you do not use parity
+
+        options.c_iflag &= ~(ICRNL | INLCR | BRKINT | IXON | IXOFF |
+                             IXANY); // Disable newline translations, break
+                                     // interrupts, and software flow control
+        options.c_oflag = 0;         // Disable all output processing
+        options.c_cflag =
+            B115200 | CS8 | CLOCAL |
+            CREAD; // 115200 baud, 8 data bits, no parity, 1 stop bit
+
+        options.c_lflag = 0; // Raw mode, no signal handling, or canonical input
+
+        options.c_cc[VMIN] = 1;  // Wait for at least 1 byte
+        options.c_cc[VTIME] = 0; // No timeout
+
+        tcsetattr(fd, TCSANOW, &options); // Apply changes immediately
+
+        // Initialize Session
+        ssize_t recv;
+        ssize_t trns;
+        unsigned char response[1];
+
+        unsigned char session_packet[6];
+        unsigned char magic[] = {0x41, 0x59};
+
+        // CRC Calculation
+
+        unsigned char* crc;
+
+        session_packet[0] = magic[0];
+        session_packet[1] = magic[1];
+        session_packet[2] = total_packets[0];
+        session_packet[3] = total_packets[1];
+
+        for(int i = 0; i < 4; i++) {
+                printf("%02X\n", session_packet[i]);
+        }
+        crc = generate_crc(session_packet, 4);
+        if(crc == NULL) {
+                printf("CRC Calculation Issue\n");
+                return -1;
+        }
+
+        session_packet[4] = crc[0];
+        session_packet[5] = crc[1];
+
+        // Log Session
+        printf("Session Initialization Log\n");
+        printf("Magic: %02X%02X\n", session_packet[0], session_packet[1]);
+        printf("Total Packets: %02X%02X\n", session_packet[2], session_packet[3]);
+        printf("CRC: %02X%02X\n", session_packet[4], session_packet[5]);
+
+        trns = write(fd, session_packet, sizeof(session_packet));
+        if (trns == -1) {
+                printf("Failed to write Session Packet\n");
+                close(fd);
+                return -1;
+        }
+
+        recv = read(fd, response, 1);
+        if (recv == -1) {
+                printf("Read Error\n");
+                close(fd);
+                return -1;
+        }
+
+        if(response[0] != session_ACK) {
+                printf("Error initializing Session. Please Restart.\n");
+                return -1;
+        }
+
+        // packet_t packet;
+        // while(total_packets_bytes--) {
+        //         unsigned char data[16];
+        //         size_t items_read = fread(data, sizeof(unsigned char), 16, firmware);
+        //         if(items_read < 16) {
+        //                 if(feof(firmware)) {
+        //                         printf("Done Reading Firmware\n");
+        //                         // Add Padding
+        //                 } else {
+        //                         printf("Error Reading\n");
+        //                         return -1;
+        //                 }
+        //         }
+        //
+        //         packet->len = sizeof(data);
+        //         packet-seqno = 
+        //
+        //         
+        // }
+        
+        printf("Yay\n");
+        close(fd);
+        fclose(firmware);
+        return 0;
+}
---
+++ libopencm3_project/script.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+if [[ "$1" == "clean" ]]; then
+        echo "Cleaning both app and bootloader..."
+        make -C app clean
+        make -C bootloader clean
+elif [[ "$1" == "json" ]]; then
+        rm -f compile_commands.json
+
+        bear -- make -C bootloader
+        bear -- make -C app
+        bear -- make -C flasher
+
+        if [[ -f compile_commands.json ]]; then
+                echo "compile_commands.json generated successfully."
+        else
+                echo "Error: Failed to generate compile_commands.json."
+                exit 1
+        fi
+
+        echo "Build completed for both bootloader and app."
+
+        echo "Cleaning build files after generating compile_commands.json..."
+        make -C app clean
+        make -C bootloader clean
+        make -C flasher clean
+
+elif [[ "$1" == "make" ]]; then
+        echo "Building both app and bootloader..."
+        make -C bootloader
+        make -C app
+elif [[ "$1" == "send" ]]; then
+        st-info --probe
+        st-flash --reset write app/firmware.bin 0x8000000
+elif [[ "$1" == "all" ]]; then
+        echo "Cleaning both app and bootloader..."
+        make -C app clean
+        make -C bootloader clean
+        echo "Building both app and bootloader..."
+        make -C bootloader
+        make -C app
+        st-info --probe
+        st-flash --reset write app/firmware.bin 0x8000000
+elif [[ "$1" == "dbg" ]]; then
+        gdb-peda-arm ./app/firmware.elf
+elif [[ "$1" == "ocd" ]]; then
+        openocd -f /usr/share/openocd/scripts/interface/stlink.cfg -f /usr/share/openocd/scripts/target/stm32f4x.cfg
+else
+        echo "Invalid argument. Use 'clean', 'json', 'make', 'all', 'dbg' or 'ocd'."
+        exit 1
+fi
